a750aa1bebd512e92d8bf4593c238d81c561ea5d
Martins M <mednis@rapidratings.com>
2017-02-20 10:43:10 +0000

    Work in progress. Page 23.

--- rango1/urls.py
+++ rango1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango_app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
