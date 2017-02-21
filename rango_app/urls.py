4837e24b43317e10da1209dddd76facf71143837
Martins M <mednis@rapidratings.com>
2017-02-21 09:24:49 +0000

    Page 28

--- rango_app/urls.py
+++ rango_app/urls.py
@@ -3,4 +3,5 @@ from rango_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
 ]
--- rango_app/views.py
+++ rango_app/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango bango!!!")
+    return HttpResponse(
+        "Rango bango!!!<br><a href=\"/rango/about/\">Go to ABOUT page</a>")
+
+def about(request):
+    return HttpResponse(
+        "This is ABOUT page. <a href=\"/rango/\">To the main Rango page</a>")
+
